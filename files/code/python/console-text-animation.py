143c3dcb697d3d31bcd5408e1aa1f9c87cd59cc1
rumyantsev168 <Rmisha08len@yandex.ru>
2025-04-29 20:18:55 +0300

    Create console-text-animation.py

---
+++ files/code/python/console-text-animation.py
@@ -0,0 +1,8 @@
+import time
+
+
+def text_art(string):
+    for i in range(len(string) + 1):
+        print('\b' * len(string), end='')
+        print(string[:i], '_' * (len(string) - i), sep='', end='')
+        time.sleep(0.25)

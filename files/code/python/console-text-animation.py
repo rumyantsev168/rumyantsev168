a5b15d516c262aaa2cdc857e90fddc57fc19c6f8
rumyantsev168 <Rmisha08len@yandex.ru>
2025-09-16 18:12:53 +0300

    Delete files/code/python directory

--- files/code/python/console-text-animation.py
+++
@@ -1,8 +0,0 @@
-import time
-
-
-def text_art(string):
-    for i in range(len(string) + 1):
-        print('\b' * len(string), end='')
-        print(string[:i], '_' * (len(string) - i), sep='', end='')
-        time.sleep(0.25)
